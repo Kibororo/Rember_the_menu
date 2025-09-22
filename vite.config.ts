d0c5480d560dcd9d20a7b2c76cbb093131f2c3c5
hamed remy <43240309+remyHamed@users.noreply.github.com>
2025-09-22 18:36:10 +0200

    fix: error vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/rh/",
+  base: "/Rember_the_menu/",
   server: {
     host: true,
     port: 5173,
@@ -17,5 +18,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
